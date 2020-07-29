d36385e37df3dacd499d9252c2be6b3e9042f706
vladyslavstartsev <17382248+vladyslavstartsev@users.noreply.github.com>
2020-07-29 15:05:12 +0300

    Update jetbrains-toolbox.sh

--- jetbrains-toolbox.sh
+++ jetbrains-toolbox.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-[ $(id -u) != "0" ] && exec sudo "$0" "$@"
-echo -e " \e[94mInstalling Jetbrains Toolbox\e[39m"
+[ "$(id -u)" != "0" ] && exec sudo "$0" "$@"
+echo " \e[94mInstalling Jetbrains Toolbox\e[39m"
 echo ""
 
 getLatestUrl() {
 USER_AGENT='User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'
-
-URL=$(curl 'https://data.services.jetbrains.com//products/releases?code=TBA&latest=true&type=release' -H 'Origin: https://www.jetbrains.com' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.8' -H "${USER_AGENT[@]}" -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Referer: https://www.jetbrains.com/toolbox/download/' -H 'Connection: keep-alive' -H 'DNT: 1' --compressed | grep -Po '"linux":.*?[^\\]",' | awk -F ':' '{print $3,":"$4}'| sed 's/[", ]//g')
-echo $URL
+JETBRAINS_RAW_URL='https://data.services.jetbrains.com//products/releases?code=TBA&latest=true&type=release'
+URL=$(curl "$JETBRAINS_RAW_URL" -H 'Origin: https://www.jetbrains.com' -H 'Accept-Encoding: gzip, deflate, br' -H 'Accept-Language: en-US,en;q=0.8' -H "${USER_AGENT[@]}" -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Referer: https://www.jetbrains.com/toolbox/download/' -H 'Connection: keep-alive' -H 'DNT: 1' --compressed | grep -Po '"linux":.*?[^\\]","' | awk -F ':' '{print $3,":"$4}'| sed 's/[", ]//g')
+echo "$URL"
 }
 getLatestUrl
 
